2c6a29442cbfbade2e46f28395d55ef54669e400
Rohit Surya <33322975+RohitSurya@users.noreply.github.com>
2021-12-30 10:24:39 +0530

    Create Zoho_Test.java
    Java Program to print pattern from middle of a string

---
+++ Zoho_Test.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		//System.out.println("Hello World");
+		//Print "WELCOME" as "COMEWEL"
+		Scanner scanner = new Scanner(System.in);
+		String test = scanner.next();
+		int length = test.length();
+		int middle = length/2;
+		char charArray[] = new char[length];
+		int counter = 0;
+		for(int i=middle; i<length;i++){
+		    charArray[counter] = test.charAt(i);
+		    counter++;
+		}
+		for(int j=0;j<middle;j++){
+		    charArray[counter] = test.charAt(j);
+		    counter++;
+		}
+		//System.out.println(charArray);
+		for(int i=0; i<test.length();i++){
+		    counter = 0;
+		    for(int j=0; j<test.length();j++){
+		        if(j<length-1)
+		            System.out.print(" ");
+		        if(j>=length-1)
+		            System.out.print(charArray[counter++]);
+		    }
+		   length--; 
+		   System.out.println();
+		}
+	}
+}
